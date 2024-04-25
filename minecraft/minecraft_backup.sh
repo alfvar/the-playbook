ab5c800e3f5544959fc0e894b16ca7be031e13ce
Alfvar <alfvar.arvidsson@gmail.com>
2024-04-25 12:06:34 +0200

    Added some error handling

--- minecraft/minecraft_backup.sh
+++ minecraft/minecraft_backup.sh
@@ -6,7 +6,7 @@ mcserver_backup_dir=/tmp/minecraft/mc_${timestamp} # Temporary backup directory 
 # Save the Minecraft world
 rconclt minecraft "save-off"
 rconclt minecraft "save-all"
-rconclt minecraft "tellraw @a {\"text\":\"<Jesper> Sparar världen...\"}"
+rconclt minecraft "tellraw @a {\"text\":\"<Jesper> Sparar världen i mc_${timestamp}\"}"
 sleep 1
 
 # Wait until save is disabled
@@ -14,12 +14,16 @@ until rconclt minecraft "save-off status" | grep -q "off"; do
     sleep 1
 done
 
-rconclt minecraft "tellraw @a {\"text\":\"<Jesper> mc_${timestamp}\"}"
-
 rm -rf /tmp/minecraft/* # Remove all old backups
 mkdir -p ${mcserver_backup_dir} # Make the directory if it doesn't exist
 docker cp minecraft:/papermc ${mcserver_backup_dir} # Copy the world data to the backup directory
 
 rconclt minecraft "save-on" # Turn saving back on
 
-# The rest is dynamically generated, and will be inserted into the end of the file when copied to the server
+# DYNAMICALLY GENERATED
+
+if [ $? -eq 0 ]; then
+    rconclt minecraft "tellraw alfvar_ {\"text\":\"<Jesper> Kopian skickades till backupservrarna.\"}"
+else
+    rconclt minecraft "tellraw @a {\"text\":\"<Jesper> Lyckades inte säkerhetskopiera. Ring Alfvar!\"}"
+fi

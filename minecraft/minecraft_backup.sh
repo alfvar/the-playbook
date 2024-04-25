dd19e4acb14b8c406aa02814ef14f2590b18b5ec
Alfvar <alfvar.arvidsson@gmail.com>
2024-04-25 13:05:57 +0200

    Added some pruning rules

--- minecraft/minecraft_backup.sh
+++ minecraft/minecraft_backup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 timestamp=$(date +%Y%m%d%H%M) # Define the timestamp variable
 mcserver_backup_dir=/tmp/minecraft/mc_${timestamp} # Temporary backup directory on the minecraft server machine
-
 # Save the Minecraft world
 rconclt minecraft "save-off"
 rconclt minecraft "save-all"
-rconclt minecraft "tellraw @a {\"text\":\"<Jesper> Sparar världen i mc_${timestamp}\"}"
+
+rconclt minecraft "tellraw @a {\"text\":\"<Jesper> Sparar världen...\"}"
 sleep 1
 
 # Wait until save is disabled
@@ -17,13 +16,32 @@ done
 rm -rf /tmp/minecraft/* # Remove all old backups
 mkdir -p ${mcserver_backup_dir} # Make the directory if it doesn't exist
 docker cp minecraft:/papermc ${mcserver_backup_dir} # Copy the world data to the backup directory
-
 rconclt minecraft "save-on" # Turn saving back on
 
 # DYNAMICALLY GENERATED
+borg_exit_status=$?
+echo "Borg exit status: $borg_exit_status" >> /home/alfvar/minecraft/logs/${timestamp}.log
 
-if [ $? -eq 0 ]; then
-    rconclt minecraft "tellraw alfvar_ {\"text\":\"<Jesper> Kopian skickades till backupservrarna.\"}"
+if [ $borg_exit_status -eq 0 ]; then
+    rconclt minecraft "tellraw alfvar_ {\"text\":\"<Jesper> Kopian skickades till backupservrarna!\"}"
 else
     rconclt minecraft "tellraw @a {\"text\":\"<Jesper> Lyckades inte säkerhetskopiera. Ring Alfvar!\"}"
 fi
+
+# Pruning repository
+info "Pruning repository"
+borg prune                          \
+    --list                          \
+    --glob-archives 'mc_*'          \
+    --show-rc                       \
+    --keep-minutely 24              \
+    --keep-daily    7               \
+    --keep-weekly   4               \
+    --keep-monthly  6
+
+prune_exit=$?
+
+# actually free repo disk space by compacting segments
+info "Compacting repository"
+borg compact
+compact_exit=$?

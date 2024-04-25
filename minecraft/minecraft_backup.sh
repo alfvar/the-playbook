fb73428e600325128d633f6072ff94a046110fb0
Alfvar <alfvar.arvidsson@gmail.com>
2024-04-25 14:29:49 +0200

    Started working on Masterplan

--- minecraft/minecraft_backup.sh
+++ minecraft/minecraft_backup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 timestamp=$(date +%Y%m%d%H%M) # Define the timestamp variable
+shorttime=$(date +%H:%M) # Define the shorttime variable
 mcserver_backup_dir=/tmp/minecraft/mc_${timestamp} # Temporary backup directory on the minecraft server machine
 # Save the Minecraft world
 rconclt minecraft "save-off"
 rconclt minecraft "save-all"
 
-rconclt minecraft "tellraw @a {\"text\":\"<Jesper> Sparar världen...\"}"
+rconclt minecraft "tellraw nobody {\"text\":\"<Jesper> Sparar världen klockan ${shorttime}\", \"color\":\"gray\"}"
 sleep 1
 
 # Wait until save is disabled
@@ -23,8 +24,8 @@ borg_exit_status=$?
 echo "Borg exit status: $borg_exit_status" >> /home/alfvar/minecraft/logs/${timestamp}.log
 
 if [ $borg_exit_status -eq 0 ]; then
-    rconclt minecraft "tellraw alfvar_ {\"text\":\"<Jesper> Kopian skickades till backupservrarna!\"}"
-else
+    rconclt minecraft "tellraw nobody {\"text\":\"<Jesper> Kopian skickades till backupservrarna!\", \"color\":\"gray\"}"
+    else
     rconclt minecraft "tellraw @a {\"text\":\"<Jesper> Lyckades inte säkerhetskopiera. Ring Alfvar!\"}"
 fi
 
